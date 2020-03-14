ef5985a50b22b65bda5e907c90b89828dd3fa4de
stag152766 <45874547+stag152766@users.noreply.github.com>
2020-03-14 17:07:34 +0700

    refactor lab5

--- lab5/run.py
+++ lab5/run.py
@@ -2,79 +2,78 @@ import numpy as np
 
 table = []
 row = []
-
+array = {}
 t_start = 0
-array_1  = {}
 
 
-def rand(time):
+def expo(time):
     average = 0
     n = 100
-
     for i in range(n):
         r = np.random.random()
         t = -time * np.log(r)
         average += (t / n)
-    return average
+    return round(average, 2)
 
 
-for i in range(10):
-    t_load = round(rand(5), 2)
-    t_road = round(rand(60), 2)
-    # номер самосвала
-    row.append(i + 1)
-    # начало загрузки
-    row.append(round(t_start, 2))
-    # конец загрузки
-    row.append(round(t_start + t_load, 2))
-    # время в пути
-    row.append(t_road)
-    # время возвращения
-    row.append(round(t_start + t_load + t_road, 2))
-    array_1[i + 1] = round(t_start + t_load + t_road, 2)
-    # порядковый номер рейса
-    row.append(i + 1)
+def set_loop(first, last, t_start):
+    for i in range(first, last):
+        t_load = expo(5)
+        t_road = expo(60)
 
-    # начало следующей загрузки
-    t_start += t_load + 0.01
-    copy_row = row[:]
-    table.append(copy_row)
-    row.clear()
+        # номер самосвала
+        if i < 10:
+            row.append(i + 1)
+        else:
+            # поиск самосвала который вернулся раньше
+            car_id = min(array, key=lambda k: array[k])
+            # номер самосвала
+            row.append(car_id - first + 10)
 
-for i in range(10, 20):
-    t_load = round(rand(5), 2)
-    t_road = round(rand(60), 2)
-    # поиск самосвала который вернулся раньше
-    car_id = min(array_1, key=lambda k: array_1[k])
-    # номер самосвала
-    row.append(car_id)
+            # формируется очередь под загрузку
+            t_back = min(array.values())
+            t_start = t_back + 0.01
+            t_finish = table[-1][2]
+            if t_start < t_finish:
+                t_start = t_finish + 0.01
 
-    # формируется очередь под загрузку
-    t_back = min(array_1.values())
-    t_start = t_back + 0.01
-    t_finish = table[-1][2]
-    if t_start < t_finish:
-        t_start = t_finish + 0.01
+        # начало загрузки
+        row.append(round(t_start, 2))
+        # конец загрузки
+        row.append(round(t_start + t_load, 2))
+        # время в пути
+        row.append(t_road)
+        # время возвращения
+        row.append(round(t_start + t_load + t_road, 2))
+        array[i + 1] = round(t_start + t_load + t_road, 2)
+        # порядковый номер рейса
+        row.append(i + 1)
 
-    # начало загрузки
-    row.append(round(t_start, 2))
-    # конец загрузки
-    row.append(round(t_start + t_load, 2))
-    # время в пути
-    row.append(t_road)
-    # время возвращения
-    row.append(round(t_start + t_load + t_road, 2))
-    array_1[i + 1] = round(t_start + t_load + t_road, 2)
-    # порядковый номер рейса
-    row.append(i + 1)
-    copy_row = row[:]
-    table.append(copy_row)
-    row.clear()
-    array_1[car_id] = 10000
+        # начало следующей загрузки
+        if i < 10:
+            t_start += t_load + 0.01
+        else:
+            array[car_id] = 10000
 
-print("%7s%12s%12s%12s%12s%12s" % ('#', 'start', 'finish', 'time', 'back', 'count'))
-for i in range(len(table)):
-    current_row = table[i]
-    for j in current_row:
-        print("%7s" % (j), end="\t\t")
-    print('\r')
+        copy_row = row[:]
+        table.append(copy_row)
+        row.clear()
+
+        if t_start >=480:
+            return
+
+
+def print_table():
+    global i
+    print("%7s%16s%16s%16s%16s%16s" % ('car_id', 't_start', 't_finish', 't_road', 't_back', 'count'))
+    for i in range(len(table)):
+        current_row = table[i]
+        for j in current_row:
+            print("%7s" % (j), end="\t\t")
+        print('\r')
+
+for r in range(10, 90, 10):
+    first = r - 10
+    last = r
+    set_loop(first, last, t_start)
+print_table()
